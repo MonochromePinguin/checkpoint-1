3e57327964a41632cde5ff0f64ada245733a65bf
monochromePinguin <jaimaille.com@gmail.com>
2018-03-31 02:38:28 +0200

    Reorganization of the HTML header (CSS priority!), admin form toggable and better stylized

--- public/index.php
+++ public/index.php
@@ -48,29 +48,19 @@ $conn = new PDOconnection();
     <!--to deactivate the old "compatibility with that old IE" mode of edge -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <link rel="stylesheet" href="default.css">
-
-
-    <!-- bootstrap links -->
-
-
-    <!-- Latest compiled and minified CSS -->
+    <!-- Latest compiled and minified boostrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
         crossorigin="anonymous">
 
-    <script
-        src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="default.css">
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous" defer></script>
   </head>
 
   <body>
+ 
+ <!-- TODO: ADD A BANNER IN CASE OF NOJS AND A NOJS.CSS -->
+
     <h1>Content of the SQL database</h1>
     <h2>obtained with PDO</h2>
 <?php
@@ -84,59 +74,110 @@ if (! $conn->getSuccess() ) {
 ?>
 
     <div class="table-responsive">
-        <table class="table table-bordered table-striped">
+        <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                    <td>Civilité</td>
-                    <td>NOM et prénom</td>
+                    <th>Civilité</th>
+                    <th>NOM et prénom</th>
                 </tr>
             </thead>
 
             <tbody>
 <?php
-while ( $data = $conn->fetchAsAssoc() )
+    while ( $data = $conn->fetchAsAssoc() )
     {
-    echo formatTableRow(
-        $data['civility'],
-        fullName($data['firstName'], $data['lastName'])
-    );
-}
+        echo formatTableRow(
+            $data['civility'],
+            fullName($data['firstName'], $data['lastName'])
+        );
+    }
 }
 
 ?>
-    <!-- #TODO : ajouter <tfoot> si nbentrées > valeur -->
+<!-- #TODO : ajouter <tfoot> si nbentrées > valeur -->
 
            </tbody>
         </table>
     </div>
 
+
     <hr>
 
-    <h2>Addition d'entrées</h2>
+    
+    <!-- this div is used to have both its children occupy same space -->
+    <div class="myContainer">
+        <label for="unhider1" class="btn btn-primary pull-left" role="button">
+            Afficher la section administrative &gt;&gt;&gt;
+        </label>
+     
+        <!-- the only role of this input is to toggle the visibility of the
+         following section -->
+        <input type="checkbox" role="aria-hidden"
+                id="unhider1" class="unhider">
+        
+        <!-- this section is shown/hidden without JS –
+         but with a combination of CSS, and a checkbox -->
+        <section class="toggableHiding">
+            <!-- a SECOND label for the same input -->
+            <label for="unhider1" class="btn btn-primary pull-left"
+                    role="button">
+                &lt;&lt;&lt; Cacher la section administrative
+            </label>
+            <br>
 
-    <form method="POST" action="#" >
-        <input type="submit" value="Ajouter une nouvelle entrée">
+            <h2>Addition d'entrées</h2>
 
-        <fieldset name="gender">
-            <legend>Genre de la personne</legend>
-            <select name="civility">
-                <option value="Fem" selected>Féminin</option>
-                <option value="Mal" selected>Masculin</option>
-                <option value="oth" selected>Autre</option>
-            </select>
-        </fieldset>
 
-        <fieldset name="firstName">
-            <legend>Prénom</legend>
-            <input name="firstName" placeholder="Prénom ..." required>
-          </fieldset>
+            <form method="POST" action="#" class="form-inline">
+ 
+                <fieldset class="form-group">
 
-        <fieldset name="lastName">
-            <legend>Nom</legend>
-            <input name="lastName" placeholder="Nom de famille ..." required>
-        </fieldset>
+                    <label for="civility">
+                        Genre
+                        <select name="civility" id="civility" required>
+                            <option disabled selected>
+                                choisir ...
+                            </option>
+                            <option value="Fem">Féminin</option>
+                            <option value="Mal">Masculin</option>
+                            <option value="oth">Autre</option>
+                        </select>
+                    </label>
+                
+                    <label for="firstName">
+                        Prénom
+                        <input name="firstName" id="firstName"
+                            placeholder="Prénom ..." required>
+                    </label>
 
-    </form>
+                    <label for="lastName">
+                        Nom
+                        <input name="lastName" id="lastName"
+                            placeholder="Nom de famille ..." required>
+                    </label>
+
+<!--TODO: make this work -->
+                    <button class="needJS" value="removeEntry" disabled>
+  <!--TODO: better label (bootstrap glyph)-->
+                         Annuler cette entrée
+                    </button>
+                </fieldset>
+
+
+                <div class="form-group">
+<!--TODO: singulier/pluriel via php -->
+                    <input type="submit" value="Stocker les entrées">
+<!--TODO: make this work -->
+                    <button class="needJS" value="addEntry" disabled>
+<!--TODO: better label (bootstrap glyph)-->
+                        Ajouter une entrée
+                    </button>
+                </div>
+
+            </form>
+
+
+        </section>
 
   </body>
 </html>
